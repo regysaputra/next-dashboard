df8c665f88cc17fa49b88fd6353405d8cbbfa72f
regysaputra <regysaputra27@gmail.com>
2024-04-26 18:38:25 +0700

    remove function from pagination.tsx

--- app/ui/invoices/pagination.tsx
+++ app/ui/invoices/pagination.tsx
@@ -52,33 +52,37 @@ export default function Pagination({ totalPages }: { totalPages: number }) {
   );
 }
 
-function PaginationNumber({
-  page,
+function PaginationArrow({
   href,
-  isActive,
-  position,
+  direction,
+  isDisabled,
 }: {
-  page: number | string;
   href: string;
-  position?: 'first' | 'last' | 'middle' | 'single';
-  isActive: boolean;
+  direction: 'left' | 'right';
+  isDisabled?: boolean;
 }) {
   const className = clsx(
-    'flex h-10 w-10 items-center justify-center text-sm border',
+    'flex h-10 w-10 items-center justify-center rounded-md border',
     {
-      'rounded-l-md': position === 'first' || position === 'single',
-      'rounded-r-md': position === 'last' || position === 'single',
-      'z-10 bg-blue-600 border-blue-600 text-white': isActive,
-      'hover:bg-gray-100': !isActive && position !== 'middle',
-      'text-gray-300': position === 'middle',
+      'pointer-events-none text-gray-300': isDisabled,
+      'hover:bg-gray-100': !isDisabled,
+      'mr-2 md:mr-4': direction === 'left',
+      '      ml-2 md:ml-4': direction === 'right',
     },
   );
 
-  return isActive || position === 'middle' ? (
-    <div className={className}>{page}</div>
+  const icon =
+    direction === 'left' ? (
+      <ArrowLeftIcon className="w-4" />
+    ) : (
+      <ArrowRightIcon className="w-4" />
+    );
+
+  return isDisabled ? (
+    <div className={className}>{icon}</div>
   ) : (
-    <Link href={href} className={className}>
-      {page}
+    <Link className={className} href={href}>
+      {icon}
     </Link>
   );
 }
